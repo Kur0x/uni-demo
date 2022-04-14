9aa76cd1da0d369d3f300fc09370b0d5c4b3914c
KuroX <dqh915@gmail.com>
2022-04-14 08:20:41 +0800

    init

---
+++ test/test.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-13 22:25:13
+ * @LastEditTime: 2022-04-13 22:25:14
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /uni-deno/test/test.js
+ */
